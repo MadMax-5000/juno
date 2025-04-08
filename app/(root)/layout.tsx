ed378227b2149efbd12b40f259b4396a8d324ca3
madmax <yassirhannaoui333@gmail.com>
2025-04-08 17:19:51 +0000

    setup app files structure and basic sign-up/in

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,7 @@
+import React, { ReactNode } from "react";
+
+const RouteLayout = ({ children }: { children: ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default RouteLayout;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>Hello this is a test to see how the sf o</div>;
+};
+
+export default Page;
